016a4e8d5dbffacb5a1733117c4079e9437449ac
George Samuel <geosam2708@gmail.com>
2023-03-21 19:14:17 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name){
+    let result1 = drivers.filter((element) => element === name);
+    let result2 = drivers.filter((element) => element === name.toLowerCase());
+    return result1.concat(result2);
+}
+
+function fuzzyMatch(drivers, someValue){
+    return drivers.filter((elemet) => elemet.startsWith(someValue));
+}
+
+function matchName(drivers, name){
+    return drivers.filter((drivers) => drivers.name === name);
+}
